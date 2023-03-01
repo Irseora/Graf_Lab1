aab5efbd6b1fcf84fb0a654a7dbee38dd815b7ae
Irseora <irseora@gmail.com>
2023-03-01 18:27:10 +0200

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace Lab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(21, 418);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(115, 20);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Graf neorientat";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(21, 444);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(100, 20);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Graf orientat";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(502, 435);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(821, 427);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(166, 32);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Retine nodurile";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1011, 482);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Name = "Form1";
+            this.Text = "Reprezentare grafuri";
+            this.Click += new System.EventHandler(this.Form1_Click);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab1
+{
+    public partial class Form1 : Form
+    {
+        Graphics g;
+        Pen p;
+        int n = 0; //nr de noduri
+        ArrayList noduri = new ArrayList(); //nodurile grafului
+        bool gata_noduri = false;
+        int arc1 = -1, arc2 = -1; //retine extremitatile unui arc (muchie)
+        int[,] a; //matricea de adiacenta
+        float raza = 20;
+
+        public Form1()
+        {
+            InitializeComponent();
+            g = CreateGraphics();
+        }
+
+        //distanta intre doua puncte
+        private double dist(PointF x, PointF y)
+        {
+            return Math.Sqrt(Math.Pow(x.X - y.X, 2) + Math.Pow(x.Y - y.Y, 2));
+        }
+
+        //gaseste un nod al grafului
+        private int find(PointF x)
+        {
+            for (int i = 0; i < n; i++)
+                if (dist(x, (PointF)noduri[i]) <= raza)
+                    return i;
+            return -1;
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            p = new Pen(Color.Black, 3);
+            PointF aux = this.PointToClient(new Point(Form1.MousePosition.X,
+            Form1.MousePosition.Y));
+            if (gata_noduri == false)
+            {
+                n++;
+                noduri.Add(aux);
+                g.DrawEllipse(p, ((PointF)noduri[n - 1]).X - raza / 2, ((PointF)noduri[n -
+                1]).Y - raza / 2, raza, raza);
+                PointF x = new PointF();
+                x.X = ((PointF)noduri[n - 1]).X - raza / 3;
+                x.Y = ((PointF)noduri[n - 1]).Y - raza / 3;
+                g.DrawString(n.ToString(), new Font(FontFamily.GenericSansSerif, 10), new
+                SolidBrush(Color.Black), x);
+            }
+            else
+            {
+                int poz = find(aux);
+                if (poz == -1)
+                    return;
+                if (arc1 == -1)
+                {
+                    arc1 = poz;
+                }
+                else
+                {
+                    arc2 = poz;
+                    a[arc1, arc2] = 1;
+
+                    //graf neorientat
+                    if (this.radioButton2.Checked == true)
+                    {
+                        a[arc2, arc1] = 1;
+                        p.EndCap = LineCap.NoAnchor;
+                    }
+                    else
+                        p.EndCap = LineCap.ArrowAnchor;
+                    //bucla
+                    if (arc1 == arc2)
+                        g.DrawArc(p, new Rectangle((int)((PointF)noduri[arc1]).X,
+                        (int)(((PointF)noduri[arc1]).Y - raza), (int)raza, (int)raza), 180, 270);
+                    else
+                        g.DrawLine(p, ((PointF)noduri[arc1]).X + raza / 2,
+                        ((PointF)noduri[arc1]).Y, ((PointF)noduri[arc2]).X + raza / 2,
+                        ((PointF)noduri[arc2]).Y);
+                    arc1 = arc2 = -1;
+
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (n == 0)
+                this.label2.Text = "Indicati nodurile grafului!";
+            else
+            {
+                gata_noduri = true;
+                a = new int[n, n];
+                for (int i = 0; i < n; i++)
+                    for (int j = 0; j < n; j++)
+                        a[i, j] = 0;
+                this.label2.Text = "Indicati arcele / muchiile!";
+            }
+        }
+    }
+}
